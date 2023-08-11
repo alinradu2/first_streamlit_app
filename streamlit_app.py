d9c50c009d07edf7e7bba6488268fef13a79528a
alinradu2 <142002576+alinradu2@users.noreply.github.com>
2023-08-11 11:57:33 +0300

    Update streamlit_app.py
    added new lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healty Diner')
+
+
+steamlit.header('Breakfast Menu')
+steamlit.text('omega3 & Blueberry Oatmeal')
+steamlit.text('Kale, Spinach & Rocket Smoothie')
+steamlit.text('Hard-Boiled Free-Range Egg')
